918c2035a80ce79ef63db74b88c63908d3c423cd
albertogg <albertogrespan@gmail.com>
2013-10-10 22:46:08 -0430

    Todo en orden para comenzar a pasar el tema.

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'https://rubygems.org'
+
+gem 'jekyll', '~> 1.2.1'
+gem 'bootstrap-sass', github: 'thomas-mcdonald/bootstrap-sass'
+gem 'compass', '~> 0.12.2'
---
+++ config.rb
@@ -0,0 +1,11 @@
+require 'bootstrap-sass'
+
+css_dir = "assets/stylesheets/"
+sass_dir = css_dir
+javascripts_dir = "assets/javascript/"
+images_dir = "assets/images/"
+fonts_dir = "assets/fonts/"
+project_type = :stand_alone
+environment = :development
+output_style = :compressed
+relative_assets = true
