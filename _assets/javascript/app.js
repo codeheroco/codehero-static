f1fc843fc75d516abde7e791a3c4b4899144d526
Jonathan Wiesel <jonathanwiesel@gmail.com>
2013-11-27 16:01:38 -0430

    agregando botones de compartir en redes sociales

--- _assets/javascript/app.js
+++ _assets/javascript/app.js
@@ -1,3 +1,6 @@
 //= require bootstrap/dropdown
 //= require bootstrap/collapse
 //= require active_link_nav
+//= require twitter-share
+//= require google-share
+//= require facebook-share
---
+++ _assets/javascript/facebook-share.js
@@ -0,0 +1,7 @@
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/es_ES/all.js#xfbml=1&appId=168533426617978";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
