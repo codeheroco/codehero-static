70a2021da633c1a9c1aa72c5da7ca5298660eb7d
albertogg <albertogrespan@gmail.com>
2013-12-16 19:09:18 -0430

    Eliminar bug con el scrollto en telefonos.
    El scrollto se estaba mostrando en el teléfono en todo momento y no debería.

--- _assets/javascript/scrollto-top.js
+++ _assets/javascript/scrollto-top.js
@@ -1,14 +1,18 @@
 $(function() {
-  $(window).scroll(function(event){
+  var viewPortWidth = $(window).width();
+
+  $(window).scroll(function(event) {
     event.preventDefault();
-    if ($(this).scrollTop() > 180) {
-      $('.scrollTo-top').fadeIn();
-    } else {
-      $('.scrollTo-top').fadeOut();
+    if (viewPortWidth > 480) {
+      if ($(this).scrollTop() > 180) {
+        $('.scrollTo-top').fadeIn();
+      } else {
+        $('.scrollTo-top').fadeOut();
+      }
     }
   });
 
-  $('.scrollTo-top').click(function(event){
+  $('.scrollTo-top').click(function(event) {
     $('html, body').animate({scrollTop : 0 }, 600);
     event.preventDefault();
   });
