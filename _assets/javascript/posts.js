34800c3405b05fefb0764bb5bab610369956a84f
albertogg <albertogrespan@gmail.com>
2014-01-07 23:36:28 -0430

    Cambio de texto al hacer click para mostrar posts.
    Se agregó el cambio de texto para el link que despliega los posts de la serie dentro del menú azul dentro de cada post.

--- _assets/javascript/posts.js
+++ _assets/javascript/posts.js
@@ -1,5 +1,6 @@
 $(function () {
   $('#show-post-list').click(function (event) {
+    $(this).text($(this).text() == 'Mostrar todos' ? 'Ocultar' : 'Mostrar todos');
     event.preventDefault();
     $('.post-ordered-list').toggle(500);
   });
