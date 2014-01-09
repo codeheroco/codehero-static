eb20e290c5bed7138a8f24c41c4ab550dc766676
albertogg <albertogrespan@gmail.com>
2014-01-09 18:56:42 -0430

    Estilar las redes sociales.
    Se estilaron las redes sociales para todos los dispositivos móviles (al fin lo logré).

--- _assets/javascript/facebook-share.js
+++ _assets/javascript/facebook-share.js
@@ -2,6 +2,6 @@
   var js, fjs = d.getElementsByTagName(s)[0];
   if (d.getElementById(id)) return;
   js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/es_ES/all.js#xfbml=1&appId=168533426617978";
+  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
